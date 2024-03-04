5754a4380ad34361faf6a0cb6f3f28e851c896a2
hackerFaruk <onuralparslan97@gmail.com>
2024-03-04 22:35:57 +0300

    it can detect two inputs from diffrent devices

--- doubleDetect.py
+++ doubleDetect.py
@@ -5,10 +5,10 @@ import multiprocessing
 
 
 def listenEvent(deviceAdress):
-    device=evdev.InputDevice('/dev/input/event1')
+    device=evdev.InputDevice(deviceAdress)
     for event in device.read_loop():
         if event.type == evdev.ecodes.EV_KEY:
-            print("halo")
+            print(device.name)
 
 
 adres1="/dev/input/event4"
@@ -17,6 +17,5 @@ adres2="/dev/input/event16"
 
 p1= multiprocessing.Process(target=listenEvent, args=(adres1,))
 p2= multiprocessing.Process(target=listenEvent, args=(adres2,))
-#p1.start()
-#p2.start()
-listenEvent(adres1)
+p1.start()
+p2.start()
