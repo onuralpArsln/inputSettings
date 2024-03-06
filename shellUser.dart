42901717b06a6ea130c05d04254dc65648e2ab66
hackerFaruk <onuralparslan97@gmail.com>
2024-03-06 11:41:21 +0300

    documentation added for sync service caller

--- shellUser.dart
+++ shellUser.dart
@@ -1,7 +1,11 @@
 import 'dart:io';
 import 'dart:convert';
 
+// bu senkron
+
 void main() async {
   var process = await Process.start('python3', ['doubleDetect.py']);
   await process.stdout.transform(utf8.decoder).forEach(print);
+
+  print("haloo darknes");
 }
